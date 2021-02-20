e7ee2060cfafab73b73a4c33f21048415498e671
Gu <936285702@qq.com>
2021-02-20 15:00:16 +0800

    函数柯里化

--- Currying/Currying.js
+++ Currying/Currying.js
@@ -1,25 +1,56 @@
-// 第一版
-var curry = function (fn) {
-  // 获取除了第一个以外的参数
-  var args = [].slice.call(arguments, 1);
-  return function () {
-    // 拼接参数
-    var newArgs = args.concat([].slice.call(arguments));
-    return fn.apply(this, newArgs);
-  };
-};
+// 第一版 只能传递两次参数，没有判断最后一个函数的参数
 
-var add = function (a, b) {
-  return a + b;
-};
+// var curry = function (fn) {
+//   // 获取除了第一个以外的参数
+//   var args = [].slice.call(arguments, 1);
+//   return function () {
+//     // 拼接参数
+//     var newArgs = args.concat([].slice.call(arguments));
+//     return fn.apply(this, newArgs);
+//   };
+// };
 
-var addCurry = curry(add, 1, 2);
-console.log(addCurry()); // 3
-//或者
-var addCurry = curry(add, 1);
-console.log(addCurry(2)); // 3
-//或者
-var addCurry = curry(add);
-console.log(addCurry(1, 2)); // 3
+// var add = function (a, b) {
+//   return a + b;
+// };
+
+// var addCurry = curry(add, 1, 2);
+// console.log(addCurry()); // 3
+// //或者
+// var addCurry = curry(add, 1);
+// console.log(addCurry(2)); // 3
+// //或者
+// var addCurry = curry(add);
+// console.log(addCurry(1, 2)); // 3
 
 // 第二版
+function sub_curry(fn) {
+  var args = [].slice.call(arguments, 1);
+  return function () {
+    return fn.apply(this, args.concat([].slice.call(arguments)));
+  };
+}
+
+function curry(fn, length) {
+  length = length || fn.length;
+
+  var slice = Array.prototype.slice;
+
+  return function () {
+    if (arguments.length < length) {
+      var combined = [fn].concat(slice.call(arguments));
+      return curry(sub_curry.apply(this, combined), length - arguments.length);
+    } else {
+      return fn.apply(this, arguments);
+    }
+  };
+}
+
+var fn = curry(function (a, b, c) {
+  return [a, b, c];
+});
+
+fn("a", "b", "c"); // ["a", "b", "c"]
+fn("a", "b")("c"); // ["a", "b", "c"]
+fn("a")("b")("c"); // ["a", "b", "c"]
+fn("a")("b", "c"); // ["a", "b", "c"]
