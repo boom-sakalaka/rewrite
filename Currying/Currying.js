226cd2fcffbb84e093af5df3fefaa0b0f385a259
Gu <936285702@qq.com>
2021-02-20 11:29:11 +0800

    添加示例/柯里化函数

--- Currying/Currying.js
+++ Currying/Currying.js
@@ -1,23 +1,25 @@
 // 第一版
 var curry = function (fn) {
+  // 获取除了第一个以外的参数
   var args = [].slice.call(arguments, 1);
   return function () {
+    // 拼接参数
     var newArgs = args.concat([].slice.call(arguments));
     return fn.apply(this, newArgs);
   };
 };
 
-var addFnc = function (a, b) {
+var add = function (a, b) {
   return a + b;
 };
 
 var addCurry = curry(add, 1, 2);
-addCurry(); // 3
+console.log(addCurry()); // 3
 //或者
 var addCurry = curry(add, 1);
-addCurry(2); // 3
+console.log(addCurry(2)); // 3
 //或者
 var addCurry = curry(add);
-addCurry(1, 2); // 3
+console.log(addCurry(1, 2)); // 3
 
 // 第二版
