c97faf4622467f1fae8c0653be9e4d73569f78ad
gzh <936285702@qq.com>
2021-04-06 10:53:11 +0800

    修改描述

--- Throttle/throttle.js
+++ Throttle/throttle.js
@@ -1,6 +1,14 @@
+/*
+ * @Author: gzh
+ * @Date: 2021-03-15 11:45:49
+ * @LastEditTime: 2021-04-06 10:53:02
+ * @LastEditors: Please set LastEditors
+ * @Description:函数节流的两种实现方法 时间戳/定时器实现
+ * @FilePath: \rewrite\Throttle\throttle.js
+ */
 // 函数节流
 
-// 第一版 使用时间戳实现
+// 第一版 使用时间戳实现 利用闭包 保存上一次执行的时间，对比当前时间，如果大于等待时间就执行
 function throttle(func, wait) {
   var context, args;
   var previous = 0;
