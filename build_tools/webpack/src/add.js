d751ad09ead1b4c2da7974f8efddfb8249af5fa7
Gu <936285702@qq.com>
2021-06-26 09:25:34 +0800

    Webpack原理以及浏览器支持的es6 module

---
+++ build_tools/webpack/src/add.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-26 08:11:45
+ * @LastEditTime: 2021-06-26 09:25:03
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \rewrite\build_tools\webpack\src\add.js
+ */
+export default function (a, b) {
+  return a + b;
+}
