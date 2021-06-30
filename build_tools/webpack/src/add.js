d4b9b3f6b0e6efed7690184df4653a56d763a58f
gzh <936285702@qq.com>
2021-06-30 11:40:48 +0800

    webpack 打包原理

---
+++ build_tools/webpack/src/add.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: GZH
+ * @Date: 2021-06-30 10:47:23
+ * @LastEditors: GZH
+ * @LastEditTime: 2021-06-30 11:01:09
+ * @FilePath: \rewrite\build_tools\webpack\src\add.js
+ */
+
+export default (a, b) => a + b;
