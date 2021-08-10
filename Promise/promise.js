c92bdf59a30b19eb82189c4821cb25f929c7e3a2
Gu <936285702@qq.com>
2021-08-10 21:19:46 +0800

    完成Promise 的 then 方法

--- Promise/promise.js
+++ Promise/promise.js
@@ -2,7 +2,7 @@
  * @Author: GZH
  * @Date: 2021-08-08 15:37:40
  * @LastEditors: GZH
- * @LastEditTime: 2021-08-08 22:46:07
+ * @LastEditTime: 2021-08-10 21:16:46
  * @FilePath: \rewrite\Promise\promise.js
  * @Description:
  */
@@ -61,21 +61,51 @@ function Promise(executor) {
 
 // 添加then方法
 Promise.prototype.then = function (onResolved, onRejected) {
-  //调用回调方法
-  if (this.PromiseState === 'fulfilled') {
-    onResolved(this.PromiseResult);
-  }
+  const self = this;
+  return new Promise((resolve, reject) => {
+    // 封装函数
+    function callback(type) {
+      try {
+        // 获取回调函数的执行结果
+        let result = type(self.PromiseResult);
+        if (result instanceof Promise) {
+          result.then(
+            v => {
+              resolve(v);
+            },
+            r => {
+              reject(r);
+            }
+          );
+        } else {
+          // 结果的返回状态为成功
+          resolve(result);
+        }
+      } catch (e) {
+        reject(e);
+      }
+    }
 
-  if (this.PromiseState === 'rejected') {
-    onRejected(this.PromiseResult);
-  }
+    //调用回调方法
+    if (this.PromiseState === 'fulfilled') {
+      callback(onResolved);
+    }
 
-  // 判断pending 状态
-  if (this.PromiseState === 'pending') {
-    // 保存回调函数
-    this.callback.push({
-      onResolved,
-      onRejected,
-    });
-  }
+    if (this.PromiseState === 'rejected') {
+      callback(onRejected);
+    }
+
+    // 判断pending 状态
+    if (this.PromiseState === 'pending') {
+      // 保存回调函数
+      this.callback.push({
+        onResolved: function () {
+          callback(onResolved);
+        },
+        onRejected: function () {
+          callback(onRejected);
+        },
+      });
+    }
+  });
 };
