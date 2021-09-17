4946c0df67b1962269191e0821bceaba6821c5b4
gzh <936285702@qq.com>
2021-09-17 16:09:56 +0800

    Golang 切片更新

--- Golang/golangStudy/8-slice/test2_slice.go
+++ Golang/golangStudy/8-slice/test2_slice.go
@@ -2,8 +2,8 @@
  * @Author: GZH
  * @Date: 2021-09-15 14:48:51
  * @LastEditors: GZH
- * @LastEditTime: 2021-09-15 15:02:20
- * @FilePath: \GoProJ\src\golangStudy\8-slice\test2_slice.go
+ * @LastEditTime: 2021-09-17 16:09:34
+ * @FilePath: \rewrite\Golang\golangStudy\8-slice\test2_slice.go
  * @Description:
  */
 package main
@@ -28,6 +28,7 @@ func main() {
 
 	fmt.Println("===========")
 
+	// 如果不想用这个参数，可以使用下划线替换，就不会有提示
 	for _, value := range myArr {
 		fmt.Println("value", value)
 	}
