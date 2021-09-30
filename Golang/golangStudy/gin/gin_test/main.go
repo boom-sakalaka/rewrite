bf75fd96fc12125118e8993151db05bda2c9082b
gzh <936285702@qq.com>
2021-09-30 11:13:36 +0800

    go 路由接受psot 参数

--- Golang/golangStudy/gin/gin_test/main.go
+++ Golang/golangStudy/gin/gin_test/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
+func main() {
+	r := gin.Default()
+	r.LoadHTMLFiles("./login.html")
+	r.GET("/login", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.html", nil)
+	})
+
+	r.POST("/login", func(c *gin.Context) {
+		userName := c.PostForm("username")
+		password := c.PostForm("password")
+
+		c.JSON(http.StatusOK, gin.H{
+			"usename":  userName,
+			"password": password,
+		})
+	})
+
+	r.Run(":9090")
 }
