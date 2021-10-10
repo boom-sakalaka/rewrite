4a716b9cfe2d0c718d1323c40f7550573019a400
Gu <936285702@qq.com>
2021-10-10 23:26:38 +0800

    添加 学习 redux 中间件的学习笔记

--- React/ReactSource/2-react-redux/src/store/index.js
+++ React/ReactSource/2-react-redux/src/store/index.js
@@ -1,16 +1,24 @@
-import {createStore, applyMiddleware, combineReducers} from "redux";
+/*
+ * @Author: GZH
+ * @Date: 2021-10-10 22:14:29
+ * @LastEditors: GZH
+ * @LastEditTime: 2021-10-10 23:25:56
+ * @FilePath: \rewrite\React\ReactSource\2-react-redux\src\store\index.js
+ * @Description:
+ */
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 // import thunk from "redux-thunk";
 // import logger from "redux-logger";
 // import promise from "redux-promise";
 // import {createStore, applyMiddleware} from "../kredux";
 
-import isPromise from "is-promise";
+import isPromise from 'is-promise';
 
 function countReducer(state = 0, action) {
   switch (action.type) {
-    case "ADD":
+    case 'ADD':
       return state + 1;
-    case "MINUS":
+    case 'MINUS':
       return state - 1;
     default:
       return state;
@@ -26,36 +34,37 @@ const store = createStore(
 
 export default store;
 
-function logger({getState}) {
+// 如果是第一个中间件的话，next 就是dispatch
+function logger({ getState }) {
   return next => action => {
-    console.log("*******************************"); //sy-log
+    console.log('*******************************'); //sy-log
 
-    console.log(action.type + "执行了！"); //sy-log
+    console.log(action.type + '执行了！'); //sy-log
 
     let prevState = getState();
-    console.log("prev state", prevState); //sy-log
+    console.log('prev state', prevState); //sy-log
 
     const returnValue = next(action);
     let nextState = getState();
-    console.log("next state", nextState); //sy-log
+    console.log('next state', nextState); //sy-log
 
-    console.log("*******************************"); //sy-log
+    console.log('*******************************'); //sy-log
     return returnValue;
   };
 }
 
 // !next就是聚合函数
 
-function thunk({dispatch, getState}) {
+function thunk({ dispatch, getState }) {
   return next => action => {
-    if (typeof action === "function") {
+    if (typeof action === 'function') {
       return action(dispatch, getState);
     }
     return next(action);
   };
 }
 
-function promise({dispatch}) {
+function promise({ dispatch }) {
   return next => action => {
     return isPromise(action) ? action.then(dispatch) : next(action);
   };
