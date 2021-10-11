199c291f22b4e0dbfe44574a71d49b25dafeb30e
Gu <936285702@qq.com>
2021-10-11 21:52:35 +0800

    添加聚合函数 compose 的示例帮助理解

--- React/ReactSource/2-react-redux/src/kredux/applyMiddleware.js
+++ React/ReactSource/2-react-redux/src/kredux/applyMiddleware.js
@@ -5,7 +5,7 @@ export default function applyMiddleware(...middlewares) {
 
     const midApi = {
       getState: store.getState,
-      dispatch: (action, ...args) => dispatch(action, ...args)
+      dispatch: (action, ...args) => dispatch(action, ...args),
     };
     const middlewareChain = middlewares.map(middleware => middleware(midApi));
 
@@ -15,7 +15,7 @@ export default function applyMiddleware(...middlewares) {
     return {
       ...store,
       //  返回加强之后的dispatch
-      dispatch
+      dispatch,
     };
   };
 }
@@ -27,5 +27,39 @@ function compose(...funcs) {
   if (funcs.length === 1) {
     return funcs[0];
   }
-  return funcs.reduce((a, b) => (...args) => a(b(...args)));
+  return funcs.reduce(
+    (a, b) =>
+      (...args) =>
+        a(b(...args))
+  );
 }
+
+/* 示例 帮助理解聚合函数 */
+
+// function f1(arg) {
+//   console.log('f1', arg);
+//   return arg;
+// }
+// function f2(arg) {
+//   console.log('f2', arg);
+//   return arg;
+// }
+// function f3(arg) {
+//   console.log('f3', arg);
+//   return arg;
+// }
+
+// function compose(...funcs) {
+//   if (funcs.length === 0) {
+//     return args => args;
+//   }
+//   if (funcs.length === 1) {
+//     return funcs[0];
+//   }
+//   return funcs.reduce((a, b) => {
+//     console.log(a, b);
+//     return (...args) => a(b(...args));
+//   });
+// }
+
+// let res = compose(f1, f2, f3)('omg');
