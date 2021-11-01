21ac8c6afd38002907e9535ba8dcd6e21d65b695
gzh <936285702@qq.com>
2021-11-01 14:26:18 +0800

    添加 eslint和prettier 自动格式化代码

---
+++ Vue/Vue-admin-template/vue-admin/.eslintrc.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: GZH
+ * @Date: 2021-11-01 14:08:58
+ * @LastEditors: GZH
+ * @LastEditTime: 2021-11-01 14:20:02
+ * @FilePath: \vue-admin\.eslintrc.js
+ * @Description:
+ */
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        'airbnb-base',
+        'plugin:prettier/recommended' // 添加 prettier 插件
+    ],
+    parserOptions: {
+        ecmaVersion: 13,
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module'
+    },
+    plugins: ['vue', '@typescript-eslint'],
+    rules: {
+        'vue/no-multiple-template-root': 'off'
+        // quotes: ['error', 'double'] // 引号规则为：“双引号”，否则一律按照 “error” 处理（你也可以改成warn试一下）
+    }
+}
--- Vue/Vue-admin-template/vue-admin/src/App.vue
+++ Vue/Vue-admin-template/vue-admin/src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: GZH
+ * @Date: 2021-11-01 10:02:12
+ * @LastEditors: GZH
+ * @LastEditTime: 2021-11-01 14:25:22
+ * @FilePath: \vue-admin\src\App.vue
+ * @Description: 
+-->
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
@@ -5,17 +13,17 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
