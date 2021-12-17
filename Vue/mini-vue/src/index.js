9d4429d2335bef1c9dca365baac074ba8e728ea2
gzh <936285702@qq.com>
2021-12-17 15:21:25 +0800

    添加mini-vue ref 的实现

--- Vue/mini-vue/src/index.js
+++ Vue/mini-vue/src/index.js
@@ -2,30 +2,38 @@
  * @Author: GZH
  * @Date: 2021-12-17 09:48:38
  * @LastEditors: GZH
- * @LastEditTime: 2021-12-17 14:38:22
+ * @LastEditTime: 2021-12-17 15:20:50
  * @FilePath: \mini-vue\src\index.js
  * @Description:
  */
-import { reactive, effect } from './reactivity';
+import { reactive, effect, ref } from './reactivity';
 
-const objValue = (window.objValue = reactive({
-  a: 1,
-  b: 2,
-  c: {
-    d: 12,
-  },
-}));
+// reactive 测试 ==========================================================
+// const objValue = (window.objValue = reactive({
+//   a: 1,
+//   b: 2,
+//   c: {
+//     d: 12,
+//   },
+// }));
 
-const arrObj = (window.arrObj = reactive([1, 2, 3]));
+// const arrObj = (window.arrObj = reactive([1, 2, 3]));
 
-effect(() => {
-  effect(() => {
-    console.warn('数组的数据' + arrObj[2]);
-  });
+// effect(() => {
+//   effect(() => {
+//     console.warn('数组的数据' + arrObj[2]);
+//   });
 
-  console.warn('数组的长度' + arrObj.length);
-});
+//   console.warn('数组的长度' + arrObj.length);
+// });
 
 // effect(() => {
 //   console.warn('数组的数据' + arrObj[4]);
 // });
+
+// ref 测试 ==================================================================
+const refVal = (window.refVal = ref('123'));
+
+effect(() => {
+  console.warn(refVal.value);
+});
--- Vue/mini-vue/src/reactivity/index.js
+++ Vue/mini-vue/src/reactivity/index.js
@@ -2,9 +2,10 @@
  * @Author: GZH
  * @Date: 2021-12-17 14:13:43
  * @LastEditors: GZH
- * @LastEditTime: 2021-12-17 14:29:59
+ * @LastEditTime: 2021-12-17 15:18:13
  * @FilePath: \mini-vue\src\reactivity\index.js
  * @Description:
  */
 export * from './reactive';
 export { effect } from './effect';
+export * from './ref';
